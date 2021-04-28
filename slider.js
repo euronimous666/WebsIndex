75b3975e881084607b2c3308f755d8c0df12180f
euronimous666 <67301201+euronimous666@users.noreply.github.com>
2021-04-28 14:29:28 -0400

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('.bxslider').bxSlider({
+      
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        slideWidth: 1280,
+    
+      
+  
+  
+    });
+  
+  });
+  
